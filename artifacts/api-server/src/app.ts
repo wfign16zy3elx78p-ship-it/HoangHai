97b95673ee11e35644fa53862447370a85456009
wfign16zy3elx78p-ship-it <wfign16zy3elx78p@gmail.com>
2026-05-03 04:41:46 +0700

    Refactor imports and update pinoHttp usage

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,31 @@
-import express, { type Express } from "express";
+import express, { type Express, Request, Response } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
-import router from "./routes";
-import { logger } from "./lib/logger";
+import * as pinoHttp from "pino-http";
+import router from "./route";
+import { logger } from "/lib/logger";
 
 const app: Express = express();
 
 app.use(
-  pinoHttp({
+  pinoHttp.default({
     logger,
     serializers: {
-      req(req) {
+      req(req: Request) {
         return {
-          id: req.id,
+          id: (req as any).id,
           method: req.method,
-          url: req.url?.split("?")[0],
+          url: req.url.split("?")[0],
         };
       },
-      res(res) {
+      res(res: Response) {
         return {
           statusCode: res.statusCode,
         };
       },
     },
-  }),
+  })
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
